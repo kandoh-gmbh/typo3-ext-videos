63bcf8d88aef8d1170d6c8f3ab28237bdde2edf2
Sven Wappler <info@wappler.systems>
2023-01-14 02:04:27 +0100

    v12 compatibility

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'mimetypes-x-content-videos_playlist' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:core/Resources/Public/Icons/T3Icons/mimetypes/mimetypes-x-content-multimedia.svg',
+    ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,5 @@
 <?php
 
-use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
-use TYPO3\CMS\Core\Imaging\IconRegistry;
 use TYPO3\CMS\Core\Resource\Rendering\RendererRegistry;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use WapplerSystems\Videos\Resource\Rendering\VideoTagRenderer;
@@ -11,16 +9,5 @@ defined('TYPO3') or die();
 
 $rendererRegistry = GeneralUtility::makeInstance(RendererRegistry::class);
 $rendererRegistry->registerRendererClass(VideoTagRenderer::class);
-
-if (TYPO3_MODE === 'BE') {
-
-    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'mimetypes-x-content-videos_playlist',
-        SvgIconProvider::class,
-        ['source' => 'EXT:core/Resources/Public/Icons/T3Icons/mimetypes/mimetypes-x-content-multimedia.svg']
-    );
-
-}
 
 
