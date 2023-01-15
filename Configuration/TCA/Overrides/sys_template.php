48f04e3c071c993ed90a69de8d66ccbc722f42b8
Sven Wappler <info@wappler.systems>
2023-01-15 22:52:18 +0100

    v12 compatibility

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,6 @@
+<?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+ExtensionManagementUtility::addStaticFile('videos', 'Configuration/TypoScript', 'Videos template');
+
--- ext_tables.php
+++ ext_tables.php
@@ -1,3 +1,2 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('videos', 'Configuration/TypoScript', 'Videos template');
