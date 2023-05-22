a1ed78c54241d253014db70512d6a56787ba0efd
Sven Wappler <info@wappler.systems>
2023-05-23 01:59:35 +0200

    update tca config

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -16,71 +16,47 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 call_user_func(
     function ($extKey, $table) {
 
-
         $newColumns = [
             'poster' => [
                 'exclude' => 1,
                 'l10n_mode' => 'mergeIfNotBlank',
                 'label' => 'LLL:EXT:videos/Resources/Private/Language/locallang_be.xlf:poster',
-                'config' => ExtensionManagementUtility::getFileFieldTCAConfig(
-                    'poster',
-                    [
-                        'minitems' => 0,
-                        'maxitems' => 1,
-                        'appearance' => [
-                            'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:media.addFileReference',
-                            'showAllLocalizationLink' => 1,
-                        ],
-                        'foreign_match_fields' => [
-                            'fieldname' => 'poster',
-                            'tablenames' => 'sys_file_metadata',
-                            'table_local' => 'sys_file',
-                        ],
-                        // custom configuration for displaying fields in the overlay/reference table
-                        // to use the newsPalette and imageoverlayPalette instead of the basicoverlayPalette
-                        'overrideChildTca' => [
-                            'types' => [
-                                File::FILETYPE_IMAGE => [
-                                    'showitem' => '
+                'config' => [
+                    'type' => 'file',
+                    'maxitems' => 1,
+                    'allowed' => 'common-image-types',
+
+                    // custom configuration for displaying fields in the overlay/reference table
+                    // to use the newsPalette and imageoverlayPalette instead of the basicoverlayPalette
+                    'overrideChildTca' => [
+                        'types' => [
+                            File::FILETYPE_IMAGE => [
+                                'showitem' => '
                                         --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette,
                                         --palette--;;imageoverlayPalette,
                                         --palette--;;filePalette'
-                                ],
                             ],
-                        ]
-                    ],
-                    $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
-                )
+                        ],
+                    ]
+                ],
             ],
             'tracks' => [
                 'exclude' => 1,
                 'l10n_mode' => 'mergeIfNotBlank',
                 'label' => 'LLL:EXT:videos/Resources/Private/Language/locallang_be.xlf:tracks',
-                'config' => ExtensionManagementUtility::getFileFieldTCAConfig(
-                    'tracks',
-                    [
-                        'minitems' => 0,
-                        'maxitems' => 10,
-                        'appearance' => [
-                            'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:media.addFileReference',
-                            'showAllLocalizationLink' => 1,
-                        ],
-                        'foreign_match_fields' => [
-                            'fieldname' => 'tracks',
-                            'tablenames' => 'sys_file_metadata',
-                            'table_local' => 'sys_file',
-                        ],
-                        'overrideChildTca' => [
-                            'types' => [
-                                File::FILETYPE_TEXT => [
-                                    'showitem' => '--palette--;LLL:EXT:lang/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.basicoverlayPalette;basicoverlayPalette,
-                                        --palette--;;filePalette,track_language,track_type'
-                                ],
+                'config' => [
+                    'type' => 'file',
+                    'maxitems' => 99,
+                    'allowed' => 'vtt',
+                    'overrideChildTca' => [
+                        'types' => [
+                            File::FILETYPE_TEXT => [
+                                'showitem' => '--palette--;LLL:EXT:lang/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.basicoverlayPalette;basicoverlayPalette,
+                                    --palette--;;filePalette,track_language,track_type'
                             ],
-                        ]
-                    ],
-                    'vtt'
-                )
+                        ],
+                    ]
+                ],
             ],
             'aspect_ratio' => [
                 'label' => 'LLL:EXT:videos/Resources/Private/Language/locallang_be.xlf:aspectRatio',
