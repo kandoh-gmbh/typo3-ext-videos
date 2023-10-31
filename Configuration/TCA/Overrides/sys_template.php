eae7ce824d06685595c609f2fc45c3665855ca1e
Sven Wappler <info@wappler.systems>
2023-10-31 13:18:59 +0100

    rename static file label

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,5 +2,5 @@
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-ExtensionManagementUtility::addStaticFile('videos', 'Configuration/TypoScript', 'Videos template');
+ExtensionManagementUtility::addStaticFile('videos', 'Configuration/TypoScript', '[videos] video player');
 
